99b951d4113a2f3bfb9ffdf71b220e55bdb512fe
Jichan <development@jc-lab.net>
2019-06-04 14:05:12 +0900

    Fix init.sh

--- init.sh
+++ init.sh
@@ -44,7 +44,7 @@ echo "driver = mysql" > /etc/dovecot/dovecot-sql.conf.ext
 echo "connect = host=$DB_HOST dbname=$DB_NAME user=$DB_USER password=$DB_PASS port=$DB_PORT" >> /etc/dovecot/dovecot-sql.conf.ext
 echo "default_pass_scheme = PBKDF2" >> /etc/dovecot/dovecot-sql.conf.ext
 echo "password_query = SELECT \`ta\`.\`email_username\` as \`username\`, \`ta\`.\`password\` as \`password\` FROM \`mail_user\` \`ta\` INNER JOIN \`mail_vdom\` \`td\` ON \`td\`.\`domain\`='%d' AND \`ta\`.\`domain_id\`=\`td\`.\`id\` WHERE \`ta\`.\`email_username\`='%n'" >> /etc/dovecot/dovecot-sql.conf.ext
-echo "user_query = SELECT 'vmail' as \`gid\`, 'vmail' as \`uid\` CONCAT('/mail-storage/', \`mailbox_id\`) as \`home\` FROM \`mail_user\` \`ta\` INNER JOIN \`mail_vdom\` \`td\` ON \`td\`.\`domain\`='%d' AND \`ta\`.\`domain_id\`=\`td\`.\`id\` WHERE \`ta\`.\`email_username\`='%n'" >> /etc/dovecot/dovecot-sql.conf.ext
+echo "user_query = SELECT 'vmail' as \`gid\`, 'vmail' as \`uid\`, CONCAT('/mail-storage/', \`mailbox_id\`) as \`home\` FROM \`mail_user\` \`ta\` INNER JOIN \`mail_vdom\` \`td\` ON \`td\`.\`domain\`='%d' AND \`ta\`.\`domain_id\`=\`td\`.\`id\` WHERE \`ta\`.\`email_username\`='%n'" >> /etc/dovecot/dovecot-sql.conf.ext
 
 chown vmail:vmail -R /mail-storage/
 
@@ -54,23 +54,23 @@ chown vmail:vmail -R /mail-storage/
 
 while true
 do
-rc_postfix=1
-rc_dovecot=1
-if [[ -f /var/spool/postfix/pid/master.pid ]]; then
-	ps -p $(cat /var/spool/postfix/pid/master.pid) >/dev/null
-	rc_postfix=$?
-fi
-
-if [[ -f /var/run/dovecot/master.pid ]]; then
-	ps -p $(cat /var/run/dovecot/master.pid) >/dev/null
-	rc_dovecot=$?
-fi
-if [[ ! $rc_postfix -eq 0 ]] || [[ ! $rc_dovecot -eq 0 ]]; then
-	echo "POSTFIX CHECK RET : $rc_postfix"
-	echo "DOVECOT CHECK RET : $rc_dovecot"
-	break
-fi
-sleep 1
+	rc_postfix=1
+	rc_dovecot=1
+	if [[ -f /var/spool/postfix/pid/master.pid ]]; then
+		ps -p $(cat /var/spool/postfix/pid/master.pid) >/dev/null
+		rc_postfix=$?
+	fi
+	
+	if [[ -f /var/run/dovecot/master.pid ]]; then
+		ps -p $(cat /var/run/dovecot/master.pid) >/dev/null
+		rc_dovecot=$?
+	fi
+	if [[ ! $rc_postfix -eq 0 ]] || [[ ! $rc_dovecot -eq 0 ]]; then
+		echo "POSTFIX CHECK RET : $rc_postfix"
+		echo "DOVECOT CHECK RET : $rc_dovecot"
+		exit 1
+	fi
+	sleep 1
 done
 
 exit 0
